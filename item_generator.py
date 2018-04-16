be6448c3feb93b1aebcd0d743ef3568a162f8dac
Spencer Morgenfeld <spencermspencerm@gmail.com>
2018-04-15 19:07:46 -0700

    Dict Fix 2.0

--- item_generator.py
+++ item_generator.py
@@ -3,9 +3,9 @@ from type import *
 
 def weaponGenerator():
     dmgMod = {
-        "Bad" : 0.9
-        "Normal" : 1
-        "Good" : 1.1
+        "Bad" : 0.9,
+        "Normal" : 1,
+        "Good" : 1.1,
     }
 
     print(dmgMod)
